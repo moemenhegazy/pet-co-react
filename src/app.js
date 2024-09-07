607bdde79f015a62eea29628aa5b28bbe059e6f9
Moemen Hegazy <moemen.hegazy@ecomexperts.com>
2024-09-08 02:02:13 +0300

    Add project configuration files and initial code structure

---
+++ src/app.js
@@ -0,0 +1,36 @@
+const Pet = (props) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("h2", {}, props.animal),
+    React.createElement("h2", {}, props.breed),
+  ]);
+};
+
+const App = () => {
+  return React.createElement(
+    'div',
+    {},
+    [
+      React.createElement('h1', {}, 'Adopt me!'),
+      React.createElement(Pet, {
+        animal: "Dog",
+        name: "Luna",
+        breed: "Havanese"
+      }),
+      React.createElement(Pet, {
+        animal: "wolf",
+        name: "wolverine",
+        breed: "Xmen"
+      }),
+      React.createElement(Pet, {
+        animal: "shark",
+        name: "bay shark",
+        breed: "great while"
+      }),
+    ]
+  );
+};
+
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+root.render(React.createElement(App));
